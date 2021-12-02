854122b1d905ddda33575c59e9429007777dc900
Roberto <r.luup.guenther@gmail.com>
2021-12-02 17:32:54 +0100

    Completed Day 2

---
+++ day2/day2_part2.py
@@ -0,0 +1,17 @@
+totalForward = 0
+totalDown = 0
+aim = 0
+
+with open("day2_input", "r") as f:
+    for i in f:
+        if i.__contains__("forward"):
+            totalForward = totalForward + int(i[8])
+            totalDown = totalDown + (int(i[8]) * aim)
+
+        elif i.__contains__("up"):
+            aim = aim - int(i[3])
+
+        elif i.__contains__("down"):
+            aim = aim + int(i[5])
+
+    print("Depth: " + str(totalForward) + " Forward: " + str(totalDown) + " Result: " + str(totalForward * totalDown))
